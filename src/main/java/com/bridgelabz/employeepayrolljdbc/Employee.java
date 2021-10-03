d9c6ecce5ca6650e10a8f3da3825add559244cc6
megha angadi <meghamangadi@gmail.com>
2021-10-03 19:21:16 +0530

    Added: Program of Employee Payroll Service to retrieve the Employee Payroll from the Database

---
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/Employee.java
@@ -0,0 +1,62 @@
+package com.bridgelabz.employeepayrolljdbc;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+	private int id;
+	private String name;
+	private char gender;
+	private LocalDate startdate;
+	private String phone;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getStartdate() {
+		return startdate;
+	}
+
+	public void setStartdate(LocalDate startdate) {
+		this.startdate = startdate;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
--- src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -1,12 +1,48 @@
 package com.bridgelabz.employeepayrolljdbc;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 public class EmployeePayrollService {
 
 	public static void main(String[] args) {
 
 		System.out.println("Welcome to the JDBC");
 
-		JdbcConnection.getConnection();
-
+		EmployeePayrollService employeepayrollservice = new EmployeePayrollService();
+		List<Employee> employeeInfor = employeepayrollservice.retrieveData();
+		employeeInfor.stream().forEach(e -> System.out.println(e.getId() + "     " + e.getName() + "    "
+				+ e.getAddress() + "     " + e.getGender() + "     " + e.getPhone() + "      " + e.getStartdate()));
 	}
+
+	public List<Employee> retrieveData() {
+		ResultSet resultSet = null;
+		List<Employee> employeeInfoList = new ArrayList<>();
+		try (Connection connection = JdbcConnection.getConnection()) {
+			Statement statement = connection.createStatement();
+			String sql = "select * from employee where id=2";
+			resultSet = statement.executeQuery(sql);
+			int count = 1;
+			while (resultSet.next()) {
+				count++;
+				Employee employeeInfo = new Employee();
+				employeeInfo.setId(resultSet.getInt("id"));
+				employeeInfo.setName(resultSet.getString("name"));
+				employeeInfo.setAddress(resultSet.getString("address"));
+				employeeInfo.setPhone(resultSet.getString("phone"));
+				employeeInfo.setGender(resultSet.getString("gender").charAt(0));
+				employeeInfo.setStartdate(resultSet.getDate("startdate").toLocalDate());
+				employeeInfoList.add(employeeInfo);
+			}
+
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+		return employeeInfoList;
+	}
+
 }
