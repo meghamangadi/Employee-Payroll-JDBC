b4b5e9988511b7b0b854fae09b01f9390b4077b4
megha angadi <meghamangadi@gmail.com>
2021-10-03 18:25:15 +0530

    Added: Program to create a payroll service database and have java program connect to database

--- src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrolljdbc/EmployeePayrollService.java
@@ -4,7 +4,7 @@ public class EmployeePayrollService {
 
 	public static void main(String[] args) {
 
-		System.out.println("Welcome to JDBC");
+		System.out.println("Welcome to the JDBC");
 
 		JdbcConnection.getConnection();
 
